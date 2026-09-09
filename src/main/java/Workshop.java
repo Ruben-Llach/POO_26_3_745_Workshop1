0f622845085f65e88f3037dfd524e092f8411a26
Ruben Llach <rallachj@udistrital.edu.co>
2026-09-08 19:53:53 -0500

    Implementar esPrimo

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -55,11 +55,23 @@ return resultado;
     }
 
     // Método que verifica si un número es primo
-    public boolean esPrimo(int numero) {
-        // TODO: Implementar el método para verificar si un número es primo.
-        // Ejemplo: Si numero = 7, el resultado debería ser true.
+public boolean esPrimo(int numero) {
+    // TODO: Implementar el método para verificar si un número es primo.
+    // Ejemplo: Si numero = 7, el resultado debería ser true.
+
+    if (numero < 2) {
         return false;
     }
+
+    for (int i = 2; i < numero; i++) {
+        if (numero % i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+    
 
     // Método que genera una serie de Fibonacci
     public int[] serieFibonacci(int n) {
