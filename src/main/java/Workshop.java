22d86e8601adeb2f871a35ba6cc64d076b8a93c0
Ruben Llach <rallachj@udistrital.edu.co>
2026-09-09 01:41:21 -0500

    Rotar n posiciones

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -295,14 +295,23 @@ public double promedioElementos(int[] arreglo) {
    
 
 
-      // Método que rota un arreglo n posiciones
+          // Método que rota un arreglo n posiciones
     public int[] rotarArreglo(int[] arreglo, int posiciones) {
         // TODO: Implementar el método para rotar un arreglo n posiciones.
         // Ejemplo: Si arreglo = [1, 2, 3, 4, 5] y posiciones = 2, el resultado debería ser [3, 4, 5, 1, 2].
-        return new int[0];
+
+        int[] resultado = new int[arreglo.length];
+
+        for (int i = 0; i < arreglo.length; i++) {
+            resultado[i] = arreglo[(i + posiciones) % arreglo.length];
+        }
+
+        return resultado;
     }
 
-    // Método que cuenta los caracteres en una cadena
+   
+
+ // Método que cuenta los caracteres en una cadena
     public int contarCaracteres(String cadena) {
         // TODO: Implementar el método para contar el número de caracteres en una cadena.
         // Ejemplo: Si cadena = "Hello", el resultado debería ser 5.
