37cf92e923878832b935ecbd47a6503511850d34
Ruben Llach <rallachj@udistrital.edu.co>
2026-09-09 22:05:45 -0500

    Rotar un arreglo n posiciones

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -296,18 +296,25 @@ public double promedioElementos(int[] arreglo) {
 
 
           // Método que rota un arreglo n posiciones
-    public int[] rotarArreglo(int[] arreglo, int posiciones) {
-        // TODO: Implementar el método para rotar un arreglo n posiciones.
-        // Ejemplo: Si arreglo = [1, 2, 3, 4, 5] y posiciones = 2, el resultado debería ser [3, 4, 5, 1, 2].
+public int[] rotarArreglo(int[] arreglo, int posiciones) {
+    // TODO: Implementar el método para rotar un arreglo n posiciones.
+    // Ejemplo: Si arreglo = [1, 2, 3, 4, 5] y posiciones = 2,
+    // el resultado debería ser [3, 4, 5, 1, 2].
 
-        int[] resultado = new int[arreglo.length];
+    int[] resultado = new int[arreglo.length];
 
-        for (int i = 0; i < arreglo.length; i++) {
-            resultado[i] = arreglo[(i + posiciones) % arreglo.length];
+    for (int i = 0; i < arreglo.length; i++) {
+        int nuevaPosicion = (i + posiciones) % arreglo.length;
+
+        if (nuevaPosicion < 0) {
+            nuevaPosicion += arreglo.length;
         }
 
-        return resultado;
+        resultado[nuevaPosicion] = arreglo[i];
     }
+
+    return resultado;
+}
 
    
 
